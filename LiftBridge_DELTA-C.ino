5ee9420b848a57b487eba48f9e496fd84237b52a
Aldrick T <taldrick@gmail.com>
2022-06-13 23:58:00 -0500

    Updated to V4.H

--- LiftBridge_DELTA-C.ino
+++ LiftBridge_DELTA-C.ino
@@ -1,6 +1,7 @@
 /*
   Lift Bridge Control System
   DELTA-C (GAMMA-2)
+  Publish ver.
   Built for Arduino MEGA 2560
   JUN 2022
 
@@ -9,7 +10,7 @@
   12JUNE2022
 
   Free use code.
-  
+
   Repository: https://github.com/aldrick-t/Lift-Bridge.git
 */
 
@@ -18,7 +19,7 @@
 #include <Chrono.h>
 #include <IRremote.h>
 
-#define closeSpeed 100
+#define closeSpeed 120
 #define openSpeed 200
 
 const int motorA_speed = 2;
@@ -76,7 +77,6 @@ Chrono lightTime;
 Servo boomGate;
 
 void setup(){
-  Serial.begin(9600);
   pinMode(motorA_speed, OUTPUT);
   pinMode(motorB_speed, OUTPUT);
 
@@ -182,6 +182,8 @@ void roadTraffic_green(){
 }
 
 void trafficReset(){
+  inputRead();
+  remoteRecieve();
   if(irsens1_status == 1 || irsens2_status == 1){
     carTime.restart();
   }
@@ -200,45 +202,24 @@ void inputRead(){
 
 void remoteRecieve(){
   remoteIn = (IrReceiver.decodedIRData.decodedRawData);
-  if (IrReceiver.decode()) {
-      Serial.println(remoteIn);
+  if (IrReceiver.decode()){
       IrReceiver.resume();
   }
-}
-
-void riverTraffic_red(){
-  digitalWrite(boatRED, HIGH);
-  digitalWrite(boatYELLOW, LOW);
-  digitalWrite(boatGREEN, LOW);
-}
-
-void riverTraffic_yellow(){
-  digitalWrite(boatRED, LOW);
-  digitalWrite(boatYELLOW, HIGH);
-  digitalWrite(boatGREEN, LOW);
-}
-
-void riverTraffic_green(){
-  digitalWrite(boatRED, LOW);
-  digitalWrite(boatYELLOW, LOW);
-  digitalWrite(boatGREEN, HIGH);
 }
 
 void loop(){
   return_loc:
   inputRead();
-  if(leafA_downState == 1 && closedTime.hasPassed(3000)){
+  if((leafA_downState == 1 && leafB_downState == 1) && closedTime.hasPassed(3000)){
     closedTime.stop();
     boomGate.write(0);
-    Serial.println("TRAFFIC GATE OPEN");
     stopLight_off();
     roadTraffic_green();
 
   }
-  else if(leafA_downState == 0){
+  else if(leafA_downState == 0 || leafB_downState == 0){
     closedTime.restart();
     boomGate.write(90);
-    Serial.println("BRIDGE LEAF AJAR");
     stopLight_hold();
   }
 
@@ -250,39 +231,28 @@ void loop(){
   if(remoteIn == 244){
     carTime.restart();
     lightTime.restart();
-    riverTraffic_yellow();
     while(lightTime.elapsed() < 5000){
       stopLight_blink();
       trafficReset();
-      Serial.println("STOP LIGHT FLASHING");
       if(lightTime.hasPassed(5000)){
         break;
       }
     }
     while(lightTime.hasPassed(4500)){
-
       lightTime.stop();
-
       boomGate.write(90);
-      Serial.println("GATE CLOSED");
-
       stopLight_hold();
-      Serial.println("STOP LIGHT HOLD");
-
       trafficReset();
       inputRead();
-      while((irsens1_status == 0 && irsens2_status == 0) && (carTime.hasPassed(10000))){
+      while((irsens1_status == 0 && irsens2_status == 0) && (carTime.hasPassed(15000))){
         inputRead();
         carTime.stop();
         liftStatus = 1;
 
-        Serial.println("BRIDGE ACTIVE");
         while(leafA_upState == 0 && liftStatus == 1){
           inputRead();
           bridgeOpen();
-          Serial.println("Opening Bridge...");
           if(leafA_upState == 1){
-            Serial.println("UPPER LIMIT REACHED");
             bridgeIdle();
             liftStatus = 0;
             break;
@@ -293,8 +263,6 @@ void loop(){
           remoteRecieve();
           inputRead();
           bridgeIdle();
-          riverTraffic_green();
-          Serial.println("BRIDGE FULLY OPEN");
           if(remoteIn == 245){
             liftStatus = 2;
             break;
@@ -302,11 +270,9 @@ void loop(){
         }
         remoteRecieve();
         while(remoteIn == 245 && liftStatus == 2){
-          riverTraffic_red();
           do{
             inputRead();
             bridgeClose();
-            Serial.println("Closing Bridge...");
             if(leafA_downState == 1){
               bridgeIdle();
               liftStatus = 3;
@@ -320,11 +286,9 @@ void loop(){
           }
         }
         if(leafA_downState == 1 && liftStatus == 3){
-          Serial.println("BRIDGE CLOSED");
           bridgeIdle();
           closedTime.restart();
           remoteIn = 0;
-          riverTraffic_red();
           roadTraffic_yellow();
           goto return_loc;
         }
